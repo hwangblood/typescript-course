e890f2a3e794375db4e664677db19b1f82e81f90
hwangblood <hwangblood@gmail.com>
2023-02-28 22:10:47 +0800

    Chapter 2: Inference, Implicit & Explicit meanings

--- 02-Basic-Types/build/js/main.js
+++ 02-Basic-Types/build/js/main.js
@@ -1,1 +1,3 @@
 "use strict";
+let myName = "hwangblood"; // Implicit string
+let myEmail = "hwangblood@gmail.com"; // Explicit string
---
+++ 02-Basic-Types/src/main.ts
@@ -0,0 +1,2 @@
+let myName = "hwangblood"; // Implicit string
+let myEmail: string = "hwangblood@gmail.com"; // Explicit string
