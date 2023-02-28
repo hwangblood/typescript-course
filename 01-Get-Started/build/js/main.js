27262415f9dfd71b11fc87d5711ab4737b8d200a
hwangblood <hwangblood@gmail.com>
2023-02-28 21:48:54 +0800

    Chapter 1: Static TYpes vs Dynamic Types

--- 01-Get-Started/build/js/main.js
+++ 01-Get-Started/build/js/main.js
@@ -1,3 +1,9 @@
 "use strict";
 let username = "hwangblood";
 console.log(`Hello ${username}`);
+let a = 12;
+// let b: number = "6";
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
--- 01-Get-Started/src/main.ts
+++ 01-Get-Started/src/main.ts
@@ -1,3 +1,11 @@
 let username = "hwangblood";
 
 console.log(`Hello ${username}`);
+
+let a: number = 12;
+// let b: number = "6";
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+console.log(c * b);
