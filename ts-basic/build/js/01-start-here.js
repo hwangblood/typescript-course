303bbce62215a4feabe21bb96b89aef1314440cb
hwangblood <hwangblood@gmail.com>
2023-03-01 19:12:09 +0800

    refactor: TS basic in one project

--- ts-basic/build/js/01-start-here.js
+++ ts-basic/build/js/01-start-here.js
@@ -7,3 +7,4 @@ let b = 6;
 let c = 2;
 console.log(a / b);
 console.log(c * b);
+// console.log(2 * "1234"); // ! The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.
--- ts-basic/src/01-start-here.ts
+++ ts-basic/src/01-start-here.ts
@@ -10,4 +10,4 @@ let c: number = 2;
 console.log(a / b);
 console.log(c * b);
 
-console.log(2 * "1234");
+// console.log(2 * "1234"); // ! The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.
