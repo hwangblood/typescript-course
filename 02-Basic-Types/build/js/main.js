4070eabf129b16e7e4f84d5aa2cf10f7506b8dd0
hwangblood <hwangblood@gmail.com>
2023-03-01 17:39:26 +0800

    Chapter 2: TS provides more information

--- 02-Basic-Types/build/js/main.js
+++ 02-Basic-Types/build/js/main.js
@@ -1,3 +1,6 @@
 "use strict";
 let myName = "hwangblood"; // Implicit string
 let myEmail = "hwangblood@gmail.com"; // Explicit string
+let school = "neusoft";
+school = "neusoft university";
+// school = 100; // ! can't assign int to  string
--- 02-Basic-Types/src/main.ts
+++ 02-Basic-Types/src/main.ts
@@ -1,2 +1,6 @@
 let myName = "hwangblood"; // Implicit string
 let myEmail: string = "hwangblood@gmail.com"; // Explicit string
+
+let school: string = "neusoft";
+school = "neusoft university";
+// school = 100; // ! can't assign int to  string
