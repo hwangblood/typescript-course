c72d2f5b618c12fe3cb52b3b62cc6f98baae19b5
hwangblood <hwangblood@gmail.com>
2023-03-13 22:48:13 +0800

    Chapter 4: Basic Functions

--- ts-basic/build/js/04-functions.js
+++ ts-basic/build/js/04-functions.js
@@ -1,8 +1,13 @@
 "use strict";
-// Type Aliases
-// Literal Types
+// * Type Aliases
+// * Literal Types
 let mName;
 // mName = 'wow'; // ! Type '"wow"' is not assignable to type '"Dave"'.
 let uname;
 uname = "Dave";
 uname = "Amy";
+// * Basic Functions
+const add = (a, b) => {
+    return a + b;
+};
+const add2 = (a, b) => a + b;
--- ts-basic/src/04-functions.ts
+++ ts-basic/src/04-functions.ts
@@ -1,4 +1,4 @@
-// Type Aliases
+// * Type Aliases
 
 type stringOrNumber = string | number;
 
@@ -12,7 +12,7 @@ type Teacher = {
 
 type UserId = string | number;
 
-// Literal Types
+// * Literal Types
 
 let mName: "Dave";
 // mName = 'wow'; // ! Type '"wow"' is not assignable to type '"Dave"'.
@@ -20,3 +20,10 @@ let mName: "Dave";
 let uname: "Dave" | "John" | "Amy";
 uname = "Dave";
 uname = "Amy";
+
+// * Basic Functions
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+const add2 = (a: number, b: number) => a + b;
