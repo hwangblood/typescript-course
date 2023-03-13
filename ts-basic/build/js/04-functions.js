2cbdf78d62afe7b29a2941d2160518d61e74ac9a
hwangblood <hwangblood@gmail.com>
2023-03-13 22:45:39 +0800

    Chapter 4: Literal Types

--- ts-basic/build/js/04-functions.js
+++ ts-basic/build/js/04-functions.js
@@ -1,2 +1,8 @@
 "use strict";
 // Type Aliases
+// Literal Types
+let mName;
+// mName = 'wow'; // ! Type '"wow"' is not assignable to type '"Dave"'.
+let uname;
+uname = "Dave";
+uname = "Amy";
--- ts-basic/src/04-functions.ts
+++ ts-basic/src/04-functions.ts
@@ -11,3 +11,12 @@ type Teacher = {
 };
 
 type UserId = string | number;
+
+// Literal Types
+
+let mName: "Dave";
+// mName = 'wow'; // ! Type '"wow"' is not assignable to type '"Dave"'.
+
+let uname: "Dave" | "John" | "Amy";
+uname = "Dave";
+uname = "Amy";
