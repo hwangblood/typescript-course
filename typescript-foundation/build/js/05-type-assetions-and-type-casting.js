59393c48b784cfc9694ef1505dcdf62de1ee5d98
hwangblood <hwangblood@gmail.com>
2023-04-02 22:45:24 +0800

    Chapter 5: Narrowing Return Values with Assertions

--- typescript-foundation/build/js/05-type-assetions-and-type-casting.js
+++ typescript-foundation/build/js/05-type-assetions-and-type-casting.js
@@ -6,3 +6,10 @@ let cc = aa; // more specific
 // * Angle Bracket Syntax instead of "as"
 let dd = "world";
 let ee = "world";
+// * Narrowing Return Values with Assertions
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
+};
+let myVal = addOrConcat(2, 3, "concat");
--- typescript-foundation/src/05-type-assetions-and-type-casting.ts
+++ typescript-foundation/src/05-type-assetions-and-type-casting.ts
@@ -10,3 +10,15 @@ let cc = aa as Three; // more specific
 // * Angle Bracket Syntax instead of "as"
 let dd = <One>"world";
 let ee = <string | number>"world";
+
+// * Narrowing Return Values with Assertions
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") return a + b;
+  return "" + a + b;
+};
+
+let myVal: string = addOrConcat(2, 3, "concat") as string;
