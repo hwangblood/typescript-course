1b3c7075bc98aa27166fc5cc745dc13a2935c79d
hwangblood <hwangblood@gmail.com>
2023-04-09 19:43:49 +0800

    Chapter 6: Larger Class

--- typescript-foundation/build/js/06-classes.js
+++ typescript-foundation/build/js/06-classes.js
@@ -1,6 +1,9 @@
 "use strict";
 class Coder {
-    constructor(name) {
+    constructor(name, music, age, lang) {
         this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
     }
 }
