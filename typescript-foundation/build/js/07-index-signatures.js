a11e9d79d4f4966eb8fce241af6cd4e2a12aca94
hwangblood <hwangblood@gmail.com>
2023-05-03 18:49:59 +0800

    Chapter 7: Why you need an index signature

--- typescript-foundation/build/js/07-index-signatures.js
+++ typescript-foundation/build/js/07-index-signatures.js
@@ -2,7 +2,32 @@
 // * What are index signatures?
 // https://dmitripavlutin.com/typescript-index-signatures/
 // * Why you need an index signature
-// * Index signature syntax
+/**
+ * The key type of this interface is 'any'
+ */
+// interface TransactionObj {
+//   Pizza: number;
+//   Books: number;
+//   Job: number;
+//   123: 123;
+// }
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+    123: 123,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+let prop = "Pizza";
+console.log(todaysTransactions[prop]);
+const todaysNet = (transctions) => {
+    let total = 0;
+    for (let prop in transctions) {
+        total += transctions[prop];
+    }
+    return total;
+};
 // * readonly modifier
 // * Non-existing properties
 // * Required properties combined with an index signature
--- typescript-foundation/src/07-index-signatures.ts
+++ typescript-foundation/src/07-index-signatures.ts
@@ -2,7 +2,39 @@
 // https://dmitripavlutin.com/typescript-index-signatures/
 
 // * Why you need an index signature
+/**
+ * The key type of this interface is 'any'
+ */
+interface TransactionObj {
+  Pizza: number;
+  Books: number;
+  Job: number;
+  123: 123;
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+  123: 123,
+};
+
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+
+let prop: string = "Pizza";
+console.log(todaysTransactions[prop]);
+
+const todaysNet = (transctions: TransactionObj): number => {
+  let total = 0;
+  for (let prop in transctions) {
+    total += transctions[prop];
+  }
+  return total;
+};
+
 // * Index signature syntax
+
 // * readonly modifier
 // * Non-existing properties
 // * Required properties combined with an index signature
