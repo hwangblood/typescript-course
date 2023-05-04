b38e63dfb84f42285cc50ed0f22c24db43ae640a
hwangblood <hwangblood@gmail.com>
2023-05-04 21:16:47 +0800

    Chapter 13: useEffect

--- typescript-react-hooks-project/README.md
+++ typescript-react-hooks-project/README.md
@@ -1,3 +1,15 @@
 # Typescript React Hooks Project
 
 Learn React Hooks with Typescript in the React + TS beginners tutorial. Do you already know React and are learning Typescript? This tutorial will help you learn how to use React hooks with Typescript.
+
+run app:
+
+```shell
+yarn
+
+yarn run dev
+```
+
+## Resources
+
+- [React 18 useEffect runs twice - Techiediaries](https://www.techiediaries.com/react-18-useeffect/)
--- typescript-react-hooks-project/src/App.tsx
+++ typescript-react-hooks-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface User {
   id: number;
@@ -7,6 +7,19 @@ interface User {
 const App = () => {
   const [count, setCount] = useState<number>(0);
   const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(
+    () => {
+      // this effect will run when the component mounts
+      console.log("mounting");
+      console.log(`Users: ${users}`);
+
+      // clean up function
+      return () => console.log("unmounting");
+    },
+    // if the users array changes, the effect will run again
+    [users]
+  );
 
   return <div className="App">App</div>;
 };
