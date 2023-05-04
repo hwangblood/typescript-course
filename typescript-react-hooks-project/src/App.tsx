f9aaa59454b3edb9c8ad90e1e35c3dfa36d5d1ac
hwangblood <hwangblood@gmail.com>
2023-05-04 21:05:34 +0800

    Chapter 13: useState

--- typescript-react-hooks-project/src/App.tsx
+++ typescript-react-hooks-project/src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
+interface User {
+  id: number;
+  username: string;
+}
 const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
   return <div className="App">App</div>;
 };
 
