dd453a9621d86153d728509815cc3681d325fa40
hwangblood <hwangblood@gmail.com>
2023-05-04 15:44:11 +0800

    Chapter 8: isObject example

--- typescript-foundation/src/08-generics.ts
+++ typescript-foundation/src/08-generics.ts
@@ -7,6 +7,21 @@ const stringEcho = (arg: string): string => arg;
 const echo = <T>(arg: T): T => arg;
 
 // * isObject example
+const isObject = <T>(arg: T): boolean => {
+  let obj = typeof arg === "object";
+  console.log(typeof arg);
+
+  let notArray = !Array.isArray(arg);
+  let notNull = arg !== null;
+  return obj && notArray && notNull;
+};
+
+console.log(isObject(true)); // boolean, sfalse
+console.log(isObject("John")); // string, false
+console.log(isObject([1, 2, 3])); // object but array, false
+console.log(isObject({ name: "John" })); // object, true
+console.log(isObject(null)); // object but null, false
+
 // * isTrue with keyof Assertion
 // * Interface with Generic example
 // * Narrowing Generics with Extends
