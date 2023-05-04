267e581adc5f5d525885791a3ed325fb82fbd4ef
hwangblood <hwangblood@gmail.com>
2023-05-04 16:29:33 +0800

    Chapter 9: Required and Readonly

--- typescript-foundation/build/js/09-utility-types.js
+++ typescript-foundation/build/js/09-utility-types.js
@@ -12,6 +12,20 @@ const assign1 = {
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded = updateAssignment(assign1, { grade: 95 });
 // * Required and Readonly
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#requiredtype
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#readonlytype
+const recordAssignment = (assign) => {
+    // send to database, etc.
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// ! Cannot assign to 'title' because it is a read-only property.
+// assignVerified.title = "new title";
+// NOTE: assignVerified won't work with recordAssignment!
+// Why? Try it and see what TS tells you :)
+// ! Argument of type 'Readonly<Assignment>' is not assignable to parameter of type 'Required<Assignment>'.
+// recordAssignment(assignVerified);
 // * Record
 // * Pick and Omit
 // * Exclude and Extract
--- typescript-foundation/src/09-utility-types.ts
+++ typescript-foundation/src/09-utility-types.ts
@@ -27,6 +27,31 @@ console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 });
 
 // * Required and Readonly
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#requiredtype
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#readonlytype
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // send to database, etc.
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+};
+
+recordAssignment({
+  ...assignGraded,
+  verified: true,
+});
+
+// ! Cannot assign to 'title' because it is a read-only property.
+// assignVerified.title = "new title";
+
+// NOTE: assignVerified won't work with recordAssignment!
+// Why? Try it and see what TS tells you :)
+// ! Argument of type 'Readonly<Assignment>' is not assignable to parameter of type 'Required<Assignment>'.
+// recordAssignment(assignVerified);
+
 // * Record
 // * Pick and Omit
 // * Exclude and Extract
