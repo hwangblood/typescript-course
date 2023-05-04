101e655a65199b316eb64e76795aadfde3265381
hwangblood <hwangblood@gmail.com>
2023-05-04 16:47:33 +0800

    Chapter 9: Awaited

--- typescript-foundation/build/js/09-utility-types.js
+++ typescript-foundation/build/js/09-utility-types.js
@@ -1,6 +1,15 @@
 "use strict";
 // * What are Utility Types?
 // https://www.typescriptlang.org/docs/handbook/utility-types.html
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const updateAssignment = (assign, propsToUpdate) => {
     return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
@@ -60,4 +69,15 @@ console.log(tsAssign);
 const assignArgs = ["Generics", 100];
 const tsAssign2 = createNewAssign(...assignArgs);
 console.log(tsAssign2);
-// * Awaited
+const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+        return res.json();
+    })
+        .catch((err) => {
+        if (err instanceof Error)
+            console.log(err.message);
+    });
+    return data;
+});
+fetchUsers().then((users) => console.log(users));
--- typescript-foundation/src/09-utility-types.ts
+++ typescript-foundation/src/09-utility-types.ts
@@ -131,4 +131,26 @@ const assignArgs: AssignParams = ["Generics", 100];
 const tsAssign2: NewAssign = createNewAssign(...assignArgs);
 console.log(tsAssign2);
 
-// * Awaited
+// * Awaited - helps us with the ReturnType of a Promise
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#awaitedtype
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      if (err instanceof Error) console.log(err.message);
+    });
+  return data;
+};
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+
+fetchUsers().then((users: FetchUsersReturnType) => console.log(users));
