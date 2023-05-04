9b4ce1fba5dc5c024c51248e5d8ced56fb1c4212
hwangblood <hwangblood@gmail.com>
2023-05-04 16:21:20 +0800

    Chapter 9: Partial

--- typescript-foundation/build/js/09-utility-types.js
+++ typescript-foundation/build/js/09-utility-types.js
@@ -1,7 +1,16 @@
 "use strict";
 // * What are Utility Types?
 // https://www.typescriptlang.org/docs/handbook/utility-types.html
-// * Partial
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: "compsci123",
+    title: "Final Project",
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
 // * Required and Readonly
 // * Record
 // * Pick and Omit
--- typescript-foundation/src/09-utility-types.ts
+++ typescript-foundation/src/09-utility-types.ts
@@ -2,6 +2,30 @@
 // https://www.typescriptlang.org/docs/handbook/utility-types.html
 
 // * Partial
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
+}
+
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment>
+): Assignment => {
+  return { ...assign, ...propsToUpdate };
+};
+
+const assign1: Assignment = {
+  studentId: "compsci123",
+  title: "Final Project",
+  grade: 0,
+};
+
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 });
+
 // * Required and Readonly
 // * Record
 // * Pick and Omit
