a853cc0fab90e4d25802c0eb071eba05e820c973
hwangblood <hwangblood@gmail.com>
2023-05-04 19:55:47 +0800

    Chapter 12: Component Return Types

--- typescript-react-project/src/components/Heading.tsx
+++ typescript-react-project/src/components/Heading.tsx
@@ -1,8 +1,12 @@
+import { ReactElement } from "react";
+
 type HeadingProps = {
   title: string;
 };
 
-const Heading = ({ title }: HeadingProps) => {
+// * Only when you return an single element, the return type can be ReactElement
+// * Otherwise, TS infer the return type to be JSX.Element
+const Heading = ({ title }: HeadingProps): ReactElement => {
   return <h1>{title}</h1>;
 };
 
