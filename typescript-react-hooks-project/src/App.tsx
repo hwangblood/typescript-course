058b0a6d6bc6f8eff67fc6355fabf52fde326bbe
hwangblood <hwangblood@gmail.com>
2023-05-16 11:00:51 +0800

    Chapter 13: useRef

--- typescript-react-hooks-project/src/App.tsx
+++ typescript-react-hooks-project/src/App.tsx
@@ -5,6 +5,7 @@ import {
   useMemo,
   MouseEvent,
   KeyboardEvent,
+  useRef,
 } from "react";
 
 interface User {
@@ -22,6 +23,8 @@ const fib: fibFunc = (n: number) => {
 const App = () => {
   const [count, setCount] = useState<number>(0);
   const [users, setUsers] = useState<User[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(
     () => {
@@ -50,12 +53,27 @@ const App = () => {
   const [myNum, setMyNum] = useState<number>(3);
   const result = useMemo<number>(() => fib(myNum), [myNum]);
 
+  const [inputValue, setInputValue] = useState<string>("");
+  const handleInput = (value: string) => {
+    setInputValue(value);
+    console.log(inputRef.current?.value); // after applying a ref, ts knowns it is not null
+  };
+
   return (
     <div className="App">
       <h1>{count}</h1>
       <button onClick={addTwo}>Add</button>
       <h2>{result}</h2>
       <button onClick={() => setMyNum((prev) => prev + 2)}>change</button>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter something..."
+          value={inputValue}
+          ref={inputRef}
+          onChange={(e) => handleInput(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
